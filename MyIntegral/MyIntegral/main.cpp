2f0f090fdd96234d3bd3f8176a34e258b6321585
tupikvtypike <tupikvtypike@gmail.com>
2017-11-18 13:29:34 +0300

    Добавлена функция замера времени

--- MyIntegral/MyIntegral/main.cpp
+++ MyIntegral/MyIntegral/main.cpp
@@ -3,6 +3,7 @@
 #include <cilk/cilk.h>
 #include <cilk/reducer_opadd.h>
 #include <iostream>
+#include <chrono>
 
 //Функция f() - подинтегральная функция
 //xi - входной параметр (точка, в которой считается подинтегральная функция)
@@ -30,10 +31,29 @@ double Integrate(int a, int b, int N)
 	return I;
 }
 
+//Функция MeanTime() - функция замера времени. Выводит время на экран. Время расчитывается как среднее (запускается программа 10000 раз и берется среднее).
+//a, b - пределы интегрирования
+//N - количество отрезков интегрирования
+//функция возвращает знчение интеграла
+void MeanTime(int a, int b, int N)
+{
+	double fullTime = 0;
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	for (int i = 0; i < 10000; ++i)
+	{
+		start = std::chrono::system_clock::now();
+		double result = Integrate(a, b, N);
+		end = std::chrono::system_clock::now();
+		fullTime += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+	}
+	std::cout << "Time: " << fullTime / 10000 << " microseconds\n";
+}
+
 int main()
 {
 	int a = 0, b = 1, N = 1000000;
 	double result = Integrate(a, b, N);
+	MeanTime(a, b, N);
 	printf("Result: %.10f\n", result);
 	system("pause");
 	return 0;
